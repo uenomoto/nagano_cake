2aeddcf038a1423e30799ae594e0fbd6775571f2
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-10-22 10:58:48 +0000

    [Add]管理者側 customerの一覧画面作りました

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,21 +2,32 @@ class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page])
   end
   
   def show
+    @customer = Customer.find(params[:id])
   end
   
   def edit
+    @customer = Customer.find(params[:id])
   end
   
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
   
   
   
   private
 
-def customer_params
+ def customer_params
 params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
-end
+ end
+ 
 end
