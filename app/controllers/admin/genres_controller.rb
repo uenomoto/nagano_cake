007882a0a898997b0757012daaaaa06c610679b3
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-10-29 04:03:01 +0000

    [Add]商品一覧画面作りました

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,4 +1,7 @@
 class Admin::GenresController < ApplicationController
+  
+  before_action :authenticate_admin!
+  
   def index
     @genres = Genre.all
     @genre = Genre.new
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,9 @@
 class Public::ItemsController < ApplicationController
+  
+  
   def index
+    @items = Item.all
+    @genres = Genre.all
   end
 
   def show
