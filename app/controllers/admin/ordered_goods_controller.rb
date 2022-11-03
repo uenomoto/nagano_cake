09bebaf78449abbe12fde8c99105a7cdfc2c65f4
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-11-03 14:43:50 +0000

    [update]注文履歴詳細画面完成しました

--- app/controllers/admin/ordered_goods_controller.rb
+++ app/controllers/admin/ordered_goods_controller.rb
@@ -1,6 +1,18 @@
 class Admin::OrderedGoodsController < ApplicationController
-  
+  before_action :authenticate_admin!
+
   def update
-    
+    @ordered_good = OrderedGood.find(params[:id])
+    @ordered_good.update(ordered_good_params)
+    @order = @ordered_good.order
+    redirect_to admin_order_path(@order.id)
   end
+
+
+  private
+
+  def ordered_good_params
+    params.require(:ordered_good).permit(:order_id, :making_status, :amount, :item_id)
+  end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,23 +1,30 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def show
-    @customer = Customer.find(params[:id])
     @order = Order.find(params[:id])
+    @ordered_goods = @order.ordered_goods
+    @total_price = @order.billing_amount - @order.postage
   end
-  
+
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
+    @order.update(status: params[:order][:status])
     redirect_to admin_order_path(@order.id)
+
+    if @order.status == "入金確認"
+      @order.ordered_goods.each do |ordered_good|
+        ordered_good.update(making_status: 1)
+      end
+    end
   end
-  
-  
-  
+
+
+
   private
-  
+
   def order_params
     params.require(:order).permit(:address, :postal_code, :name, :billing_amount, :postage, :method_of_payment, :status, :making_status)
   end
-  
+
 end
