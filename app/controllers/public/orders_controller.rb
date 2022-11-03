e333b19a1ec9c982dfc8b24ec2911d1e65f9b4b1
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-11-03 12:35:38 +0000

    [update]カスタマー注文一覧と詳細画面完成しました

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,6 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def new
     @customer = current_customer
     @order = Order.new
@@ -8,37 +8,42 @@ class Public::OrdersController < ApplicationController
 
   def index
     @orders =Order.where(customer:current_customer)
-    @ordered_goods = OrderedGood.where(customer:current_customer)
   end
 
   def show
-    #@order = Order.find(params[:id])
-    #@ordered_goods = @order.ordered_goods
+    @order = Order.find(params[:id])
+    @ordered_goods = @order.ordered_goods
+    #合計金額　＝　請求額　ー　送料
+    @total_price = @order.billing_amount - @order.postage
   end
-  
+
   def confirm
     @cart_items = current_customer.cart_items
-    
+
    #合計金額
-      @total_price =
-       total_price = 0
+      @total_price = 0
        @cart_items.each do |cart_item|
-      total_price += cart_item.subtotal
+         subtotal = cart_item.item.with_tax_price * cart_item.amount
+      @total_price += subtotal
       end
-      
-    
+
+
     #paramsメソッドで直接取得
     @order = Order.new(
       customer: current_customer,
       method_of_payment: params[:order][:method_of_payment])
-      
+
+      #請求金額（合計金額＋送料）
+      @order.billing_amount = @total_price + @order.postage
+
+
       #残りのorder_params取得↓
        #自分の住所
     if params[:order][:select_address] == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.full_name
-      
+
       #配送先一覧の住所
     elsif params[:order][:select_address] == "1"
       #ここで配送先モデルの１つのアドレスIDレコードを取得↓
@@ -46,7 +51,7 @@ class Public::OrdersController < ApplicationController
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
-      
+
       #新配送先
     elsif params[:order][:select_address] == "2"
       @order.postal_code = params[:order][:postal_code]
@@ -56,20 +61,20 @@ class Public::OrdersController < ApplicationController
     else
       render :new
     end
-    
+
   end
-  
+
   def create
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
-    params[:order][:billing_amount] = @order.billing_amount
+    @order.postage = 800
     @order.save
-    
+
     #情報入力時新配送先が選ばれたら配送先保存↓
     if params[:order][:new_address] =="1"
       current_customer.address.create(address_params)
     end
-    
+
     #注文商品(Order_goods)モデルにカート内商品の情報をもとに保存
     @cart_items = current_customer.cart_items
     @cart_items.each do |cart_item|
@@ -80,25 +85,25 @@ class Public::OrdersController < ApplicationController
       @ordered_good.tax_included_price = cart_item.item.with_tax_price * cart_item.amount
       @ordered_good.save
     end
-    
+
     #最後にカート内商品を全て削除
     @cart_items.destroy_all
-    
-    
+
+
     redirect_to complete_path
   end
-  
-  
-  
+
+
+
 
   private
-  
+
   def order_params
-    params.require(:order).permit(:postal_code, :address, :name, :billing_amount)
+    params.require(:order).permit(:postal_code, :address, :name, :billing_amount, :method_of_payment)
   end
-  
+
   def address_params
     params.require(:order).permit(:postal_code, :name, :address)
   end
-  
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -8,24 +8,11 @@ class Order < ApplicationRecord
   
   enum status: { "入金待ち": 0, "入金確認": 1, "制作中": 2, "発送準備中": 3, "発送済み": 4 }
   
-  #送料
-  def postage
-    800
-  end
+  
+ #送料 
+def postage
+  800
+end
+  
 
-#合計金額
-  def total_price
-       total_price = 0
-       cart_items.each do |cart_item|
-       total_price += cart_item.subtotal
-      end
-      #返り値（メソッド処理終了、式終了）
-     return total_price
-  end
-  
-  
-   #請求金額
-  def billing_amount
-    total_price + postage
-  end
 end
