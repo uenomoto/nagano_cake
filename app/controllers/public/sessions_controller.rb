6e20cb641ef568bb70ccf578c6248c83eb698321
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-11-03 05:11:34 +0000

    [update]理論削除したユーザーをログインできなくしました

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :customer_state, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,8 +18,22 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
-
+  protected
+  
+# 退会しているかを判断するメソッド
+  def customer_state
+      ##入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+   if @customer
+    ## 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別かつ退会ステータスがtrueだったら
+    if (@customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true))
+      redirect_to new_customer_registration_path
+    end
+   
+      
+   end
+  end
+  
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
